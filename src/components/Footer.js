93aa9bd7a2ca7e89d8ac0207e8499a601188c26a
Carlos Miguel Samson <114665584+MIGSNOLIMIT@users.noreply.github.com>
2026-06-03 01:50:14 +0800

    modified:   src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiFillGithub, AiOutlineMail } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import PortfolioViewCounter from "./Home/PortfolioViewCounter";
 
 function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
+          <PortfolioViewCounter />
           <h3>Designed and developed by Carlos Miguel Samson</h3>
         </Col>
         <Col md="4" className="footer-copywright">
